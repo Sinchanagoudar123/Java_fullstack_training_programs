059accfa5395d8ef21e3d69dd5d75c1c864d59b8
Sinchana goudar <96060457+Sinchanagoudar123@users.noreply.github.com>
2023-10-06 18:08:30 +0530

    Create AndroidSeries.java

---
+++ AndroidSeries.java
@@ -0,0 +1,10 @@
+package annotation;
+@SmartTv(Os="Android",height=40,width=50)
+public class AndroidSeries {
+ String model;
+ int ScreenSize;
+ public AndroidSeries(String model,int ScreenSize) {
+	 this.model=model;
+	 this.ScreenSize=ScreenSize;
+ }
+}
