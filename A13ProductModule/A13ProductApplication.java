34636a2a9f0be06584c6152b32c9cd58a92f2a9a
Sinchana goudar <96060457+Sinchanagoudar123@users.noreply.github.com>
2023-10-31 20:13:20 +0530

    Create A13ProductApplication.java

---
+++ A13ProductModule/A13ProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A13ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A13ProductApplication.class, args);
+	}
+
+}
